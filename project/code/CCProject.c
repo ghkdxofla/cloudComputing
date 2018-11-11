89f5fb76819768cde7c5192674b0c9840f005823
Taelim Hwang <ghkdxofla@gmail.com>
2018-11-11 10:35:57 +0900

    refactoring codes

--- project/code/CCProject.c
+++ project/code/CCProject.c
@@ -17,12 +17,23 @@ typedef struct _CpuState{
 	double cpu_usage;
 } CpuState;
 
-void* save_xentop(void* n){
-	fp = popen("xentop -b -d 1", "r");
-	while(1){
-		//Do nothing
-	}
-	pclose(fp);
+void print_time();
+void* open_xentop(void*);
+void* print_xentop(void*);
+void dvam_xentop();
+
+int main(int argc, char *argv[]){
+	printf("--PROGRAM START--\n");
+	
+	pthread_t p_thread[2];
+	pthread_create(&p_thread[0], NULL, open_xentop, NULL);
+	pthread_create(&p_thread[1], NULL, print_xentop, NULL);
+	
+	pthread_join(p_thread[0], NULL);
+	pthread_join(p_thread[1], NULL);
+
+	return 0;
+  
 }
 
 void print_time(){
@@ -38,10 +49,21 @@ void print_time(){
 	printf("[time] %d-%d-%d (%s) %d:%d:%d\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, day_kor[t->tm_wday], t->tm_hour, t->tm_min, t->tm_sec);
 }
 
+void* open_xentop(void* n){
+	fp = popen("xentop -b -d 1", "r");
+	while(1){
+		//Do nothing
+	}
+	pclose(fp);
+}
+
 void* print_xentop(void* n){
 	// xentop info
 	char line[10240];
+	char line_count[10240];
+	int init_count = -1;
 	int vpm_count = -1;
+	FILE* fp_count = NULL;
 	CpuState cpu_state;
 	
 	while(1){
@@ -54,8 +76,12 @@ void* print_xentop(void* n){
 			memset(check_name, 0, sizeof(check_name));
 			sscanf(line, "%s", check_name);
 			if(strcmp(check_name, "NAME") == 0){
-				if(vpm_count != -1) 
-					printf("[VM count] : %d\n", ++vpm_count);
+				init_count = -1;
+				fp_count = popen("xl vm-list", "r");
+				while(fgets(line_count, 10240, fp_count) != NULL){
+					init_count++;
+				}
+
 				print_time();
 				vpm_count = 0;
 				continue;
@@ -66,23 +92,10 @@ void* print_xentop(void* n){
 			sscanf(line, "%s %s %d %lf", cpu_state.name, cpu_state.state, &cpu_state.cpu_time, &cpu_state.cpu_usage);
 			printf("[VM] %s %s %d %.1lf\n", cpu_state.name, cpu_state.state, cpu_state.cpu_time, cpu_state.cpu_usage);
 			vpm_count++;
-			
+			if(vpm_count == init_count)
+				printf("[VM count] : %d\n", vpm_count);
 			
 		}
 		
 	}
-}
-
-int main(int argc, char *argv[]){
-	printf("--PROGRAM START--\n");
-	
-	pthread_t p_thread[2];
-	pthread_create(&p_thread[0], NULL, save_xentop, NULL);
-	pthread_create(&p_thread[1], NULL, print_xentop, NULL);
-	
-	pthread_join(p_thread[0], NULL);
-	pthread_join(p_thread[1], NULL);
-
-	return 0;
-  
 }
