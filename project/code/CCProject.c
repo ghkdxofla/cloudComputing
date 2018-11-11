86bae50986bd1216baf93c110136857d83081a89
Taelim Hwang <ghkdxofla@gmail.com>
2018-11-11 10:14:33 +0900

    make a function for printing in CLI

--- project/code/CCProject.c
+++ project/code/CCProject.c
@@ -3,8 +3,19 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Load xentop command line into file pointer
 FILE * fp = NULL;
+char check_name[20];
+
+typedef struct _CpuState{
+	char name[10];
+	char state[8];
+	int cpu_time;
+	double cpu_usage;
+} CpuState;
 
 void* save_xentop(void* n){
 	fp = popen("xentop -b -d 1", "r");
@@ -14,16 +25,51 @@ void* save_xentop(void* n){
 	pclose(fp);
 }
 
-void* print_xentop(void* n){
-	char line[10240]; // 10240
+void print_time(){
+	time_t timer;
+	struct tm *t;
+	char* day_kor[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	
+	
+	timer = time(NULL);
+	t = localtime(&timer);
+	
+	printf("=======================================\n");
+	printf("[time] %d-%d-%d (%s) %d:%d:%d\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, day_kor[t->tm_wday], t->tm_hour, t->tm_min, t->tm_sec);
+}
 
+void* print_xentop(void* n){
+	// xentop info
+	char line[10240];
+	int vpm_count = -1;
+	CpuState cpu_state;
+	
 	while(1){
 		while(fp == NULL){
 			//Do nothing
 		}
 		while(fgets(line, 10240, fp) != NULL){
-			printf("%s", line);
+			//printf("%s", line);
+			
+			memset(check_name, 0, sizeof(check_name));
+			sscanf(line, "%s", check_name);
+			if(strcmp(check_name, "NAME") == 0){
+				if(vpm_count != -1) 
+					printf("[VM count] : %d\n", ++vpm_count);
+				print_time();
+				vpm_count = 0;
+				continue;
+			}
+			
+			else if(strcmp(check_name, "Domain-0") == 0) continue;
+
+			sscanf(line, "%s %s %d %lf", cpu_state.name, cpu_state.state, &cpu_state.cpu_time, &cpu_state.cpu_usage);
+			printf("[VM] %s %s %d %.1lf\n", cpu_state.name, cpu_state.state, cpu_state.cpu_time, cpu_state.cpu_usage);
+			vpm_count++;
+			
+			
 		}
+		
 	}
 }
 
