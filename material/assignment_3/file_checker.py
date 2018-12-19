f4851eec0f3a10d9ce882db9ac7dd9f7f7de77e3
Taelim Hwang <ghkdxofla@gmail.com>
2018-12-19 21:50:03 +0900

    modify check file code

--- material/assignment_3/file_checker.py
+++ material/assignment_3/file_checker.py
@@ -3,51 +3,57 @@ from time import time, sleep
 
 class CheckFile(object):
     def __init__(self):
-        self._cache_stamp = 0
         self.data_path = "/data"
         self.cache_name = ".last"
+        self.last_time = 0
         try:
             with open(self.data_path+'/'+self.cache_name, "r") as f:
                 lines = f.readlines()
                 self.file_length = len(lines)
                 try:
-                    self.first_time = int(lines[0].split(",")[0])
+                    self.first_time = int(lines[0].split(",")[0]) 
                 except:
                     self.first_time = 0
         except:
             self.file_length = 0
+
         
 
     def check(self):
+        try:
+            with open(self.data_path+'/'+self.cache_name, "r") as f:
+                lines = f.readlines()
+                new_length = len(lines)
+                try:
+                    last_time = int(lines[-1].split(",")[0])
+                except:
+                    last_time = 0
+                try:
+                    first_time = int(lines[0].split(",")[0]) 
+                except:
+                    first_time = 0
+        except:
+            return
+
+        if last_time > self.last_time:
+            with open(self.data_path+'/'+".last_log", "a") as f:
+                if self.first_time < first_time:
+                    f.write("["+str(int(time()))+"]"+" --- .last file newly created!\n")
+                    for line in lines:
+                        f.write(line+"\n")
+                    f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length)+" log data!\n")
+                    self.first_time = first_time
+
+                else:
+                    for line in lines[self.file_length:new_length]:
+                        f.write(line+"\n")
+                    f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length - self.file_length)+" log data!\n")
+
+                self.file_length = new_length
+                self.last_time = last_time
+
         
-        stamp = os.stat(self.data_path+'/'+self.cache_name).st_mtime
 
-        if stamp != self._cache_stamp:
-            self._cache_stamp = stamp
-            try:
-                with open(self.data_path+'/'+self.cache_name, "r") as f:
-                    lines = f.readlines()
-                    new_length = len(lines)
-                    try:
-                        last_time = int(lines[-1].split(",")[0])
-                    except:
-                        last_time = 0
-                with open(self.data_path+'/'+".last_log", "a") as f:
-                    if self.first_time > last_time:
-                        f.write("["+str(int(time()))+"]"+" --- .last file newly created!\n")
-                        f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length)+" log data!\n")
-                        for line in lines:
-                            f.write(line+"\n")
-                        
-                    else:
-                        f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length - self.file_length)+" log data!\n")
-                        for line in lines[self.file_length:new_length]:
-                            f.write(line+"\n")
-                    self.file_length = new_length
-                    self.first_time = last_time
-
-            except:
-                pass
 
 if __name__ == '__main__':
     check_file = CheckFile()
