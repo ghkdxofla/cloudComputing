b19b2146553f62f013377fe6df49989710a5d1fa
Taelim Hwang <ghkdxofla@gmail.com>
2018-12-19 13:22:51 +0900

    add check file code

---
+++ material/assignment_3/assignment_2-2.dockerfile
@@ -0,0 +1,19 @@
+# Get docker image(alpine linux) --- layer 1
+FROM alpine:latest
+# Set working directory(/usr/src/app). This dir is used by Flask --- layer 2
+WORKDIR /usr/src/app
+
+# Run terminal command --- layer 3(execute all commands once in a layer. If not, it takes a lot of time)
+# wget {url} : install files for running node.js server(1 file needed)
+# apk add {package} : install packages(python3, pip needed)
+# pip3 install flask : install Flask
+RUN wget https://raw.githubusercontent.com/ghkdxofla/cloudComputing/master/material/assignment_3/app.py \
+&& apk update \
+&& apk add python3 \
+&& apk add py-pip \
+&& pip3 install flask
+
+# Open 8080 port
+EXPOSE 8080
+# Run Flask server after configuration(if not, docker is closed immediately)
+CMD ["python3", "app.py"]
--- material/assignment_3/file_checker.py
+++ material/assignment_3/file_checker.py
@@ -1,24 +1,52 @@
 import os
+from time import time, sleep
 
 class CheckFile(object):
     def __init__(self):
         self._cache_stamp = 0
         self.data_path = "/data"
         self.cache_name = ".last"
+        try:
+            with open(self.data_path+'/'+self.cache_name, "r") as f:
+                lines = f.readlines()
+                self.file_length = len(lines)
+                try:
+                    self.first_time = int(lines[0].split(",")[0])
+                except:
+                    self.first_time = 0
+        except:
+            self.file_length = 0
+        
 
     def check(self):
-        total_path = self.data_path+'/'+self.cache_name
+        
         stamp = os.stat(total_path).st_mtime
 
         if stamp != self._cache_stamp:
             self._cache_stamp = stamp
-            with open(total_path, "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    print(line)
+            try:
+                with open(self.data_path+'/'+self.cache_name, "r") as f:
+                    lines = f.readlines()
+                    new_length = len(lines)
+                    try:
+                        last_time = int(lines[-1].split(",")[0])
+                    except:
+                        last_time = 0
+                with open(self.data_path+'/'+".last_log", "w") as f:
+                    if self.first_time > last_time:
+                        f.write("["+str(int(time()))+"]"+" --- .last file newly created!")
+                        f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length)+"log data!")
+                        
+                    else:
+                        f.write("["+str(int(time()))+"]"+" --- .added "+str(new_length - self.file_length)+"log data!")
+                    self.file_length = new_length
+                    self.first_time = last_time
 
+            except:
+                pass
 
 if __name__ == '__main__':
     check_file = CheckFile()
     while True:
         check_file.check()
+        sleep(3)
